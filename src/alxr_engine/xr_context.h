5be4ef4f98a10276e9731d4a76a336cdadd560e7
korejan <64199710+korejan@users.noreply.github.com>
2024-06-26 22:03:19 +0100

    Adds support for XR_EXT_hand_tracking_data_source

--- src/alxr_engine/xr_context.h
+++ src/alxr_engine/xr_context.h
@@ -61,6 +61,28 @@ struct XrContext {
 	}
 };
 
+template <typename XrTargetT, typename XrSourceInT>
+constexpr inline const XrTargetT* GetChained(const XrSourceInT& val, const XrStructureType stuctType) {
+	for (auto curr = reinterpret_cast<const XrBaseInStructure*>(val.next);
+		curr != nullptr; curr = curr->next) {
+		if (curr->type == stuctType) {
+			return reinterpret_cast<const XrTargetT*>(curr);
+		}
+	}
+	return nullptr;
+}
+
+template <typename XrTargetT, typename XrSourceOutT>
+constexpr inline XrTargetT* GetChained(XrSourceOutT& val, const XrStructureType stuctType) {
+	for (auto curr = reinterpret_cast<XrBaseOutStructure*>(val.next);
+		curr != nullptr; curr = curr->next) {
+		if (curr->type == stuctType) {
+			return reinterpret_cast<XrTargetT*>(curr);
+		}
+	}
+	return nullptr;
+}
+
 constexpr inline const char* ToString(const XrRuntimeType t) {
 	switch (t) {
 	case XrRuntimeType::SteamVR:   return "SteamVR";
