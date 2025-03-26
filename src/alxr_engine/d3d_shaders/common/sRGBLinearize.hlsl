e9a0b5fc8b0974912a51b403475bac6303c1ad8a
korejan <64199710+korejan@users.noreply.github.com>
2025-03-26 22:33:19 +0000

    shader optimizations for sRGB linearization

--- src/alxr_engine/d3d_shaders/common/sRGBLinearize.hlsl
+++ src/alxr_engine/d3d_shaders/common/sRGBLinearize.hlsl
@@ -11,14 +11,21 @@ float3 ConvertYUVtoRGB(float3 ycbcr) {
 // conversion based on: https://www.khronos.org/registry/DataFormat/specs/1.4/dataformat.1.4.html#TRANSFER_SRGB
 float3 sRGBToLinearRGB(float3 srgb)
 {
-    static const float delta = 1.0 / 12.92;
-    static const float alpha = 1.0 / 1.055;
-    static const float3 delta3  = float3(delta, delta, delta);
-    static const float3 alpha3  = float3(alpha, alpha, alpha);
-    static const float3 theta3  = float3(0.04045, 0.04045, 0.04045);
-    static const float3 offset3 = float3(0.055, 0.055, 0.055);
-    static const float3 gamma3  = float3(2.4, 2.4, 2.4);
-    const float3 lower = srgb * delta3;
-    const float3 upper = pow((srgb + offset3) * alpha3, gamma3);
+    static const float delta   = 1.0 / 12.92;
+    static const float alpha   = 1.0 / 1.055;
+    static const float beta    = 0.055 / 1.055;
+    static const float3 alpha3 = float3(alpha, alpha, alpha);
+    static const float3 theta3 = float3(0.04045, 0.04045, 0.04045);
+    static const float3 beta3  = float3(beta, beta, beta); // Precomputed (alpha3 * 0.055)
+    static const float3 gamma3 = float3(2.4, 2.4, 2.4);
+
+    const float3 lower = srgb * delta;
+    // alpha3 * (srgb + offset3)
+    //      == (alpha3 * srgb) + (alpha3 * offset3)
+    //      == (alpha3 * srgb) + beta3
+    //      == mad(alpha3, srgb, beta3)
+    //  where
+    //    const float3 offset3 = float3(0.055,0.055,0.055);
+    const float3 upper = pow((alpha3 * srgb) + beta3, gamma3);
     return lerp(lower, upper, srgb > theta3);
 }
--- src/alxr_engine/vulkan_shaders/common/sRGBLinearize.glsl
+++ src/alxr_engine/vulkan_shaders/common/sRGBLinearize.glsl
@@ -1,16 +1,22 @@
 precision highp float;
 
-const vec3 delta3  = vec3(1.0 / 12.92);
+const float delta  = (1.0 / 12.92);
 const vec3 alpha3  = vec3(1.0 / 1.055);
 const vec3 theta3  = vec3(0.04045);
-const vec3 offset3 = vec3(0.055);
+const vec3 beta3   = vec3(0.055 / 1.055); // Precomputed (alpha3 * 0.055)
 const vec3 gamma3  = vec3(2.4);
 
-// conversion based on: https://www.khronos.org/registry/DataFormat/specs/1.3/dataformat.1.3.html#TRANSFER_SRGB
+// conversion based on: https://www.khronos.org/registry/DataFormat/specs/1.4/dataformat.1.4.html#TRANSFER_SRGB
 vec4 sRGBToLinearRGB(vec4 srgba)
 {
     const vec3 srgb = srgba.rgb;
-    const vec3 lower = srgb * delta3;
-    const vec3 upper = pow(alpha3 * (srgb + offset3), gamma3);
+    const vec3 lower = srgb * delta;
+    // alpha3 * (srgb + offset3)
+    //      == (alpha3 * srgb) + (alpha3 * offset3)
+    //      == (alpha3 * srgb) + beta3
+    //      == mad(alpha3, srgb, beta3)
+    //  where
+    //    const vec3 offset3 = vec3(0.055);
+    const vec3 upper = pow(fma(alpha3, srgb, beta3), gamma3);
     return vec4(mix(lower, upper, greaterThan(srgb, theta3)), srgba.a);
 }
